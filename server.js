bfa93015ad893315cf1a3e303868608026cc47c5
Tim Mendoza <tmendoza@twilio.com>
2020-08-12 16:56:46 -0600

    Adjust for heroku

--- server.js
+++ server.js
@@ -26,4 +26,6 @@ app.get('/token', (req, res) => {
 
 app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
 
-app.listen(8081, () => console.log('token server running on 8081'));
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => console.log(`token server running on ${port}`));
